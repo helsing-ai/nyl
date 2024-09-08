a6188d2057daefdf25f89533403e7f9c15b1f26a
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-09-08 13:31:36 +0200

    feature: Add `nyl profile get-kubeconfig` command

--- src/nyl/commands/profile.py
+++ src/nyl/commands/profile.py
@@ -25,3 +25,15 @@ def activate(profile_name: str = Argument("default", envvar="NYL_PROFILE")) -> N
 
     for key, value in profile.env.items():
         print(f"export {key}={shlex.quote(value)}")
+
+
+@app.command()
+def get_kubeconfig(profile_name: str = Argument("default", envvar="NYL_PROFILE")) -> None:
+    """
+    Similar to `nyl profile activate`, but prints only the path to the `KUBECONFIG` file.
+    """
+
+    with ProfileManager.load() as manager:
+        profile = manager.activate_profile(profile_name)
+
+    print(profile.kubeconfig)
