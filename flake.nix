90f0da2de34f9f045335632d56c1a33b647fe9a0
Niklas Rosenstein <niklas.rosenstein@helsing.ai>
2024-11-07 18:25:26 +0100

    wrap docs building in Nix

--- flake.nix
+++ flake.nix
@@ -23,17 +23,13 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python312;
-        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-        overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
-        pyprojectOverrides = _final: _prev:
-          {
-            # Implement build fixups here.
-          };
-        pythonSet = (pkgs.callPackage pyproject-nix.build.packages {
-          inherit python;
-        }).overrideScope
-          (nixpkgs.lib.composeExtensions overlay pyprojectOverrides);
+        uv = pkgs.callPackage ./nix/uv.nix { inherit uv2nix pyproject-nix; };
+
+        uvProject = uv.loadProject {
+          workspaceRoot = ./.;
+          python = pkgs.python312;
+          # TODO: Pass runtimeInputs here?
+        };
 
         mypyConfig = pkgs.writeText "mypy.ini" ''
           [mypy]
@@ -48,35 +44,47 @@
           line-length = 120
         '';
 
-        devEnv = pythonSet.mkVirtualEnv "dev" workspace.deps.all;
-        dmypy = "${devEnv}/bin/dmypy";
-        ruff = "${devEnv}/bin/ruff";
-        pytest = "${devEnv}/bin/pytest";
-
         dependencies =
           [ pkgs.kubernetes-helm pkgs.kyverno pkgs.sops pkgs.kubectl ];
       in rec {
         packages.default =
-          (pythonSet.mkVirtualEnv "nyl" workspace.deps.default).overrideAttrs
-          (oldAttrs: {
-            runtimeInputs = oldAttrs.runtimeInputs or [ ] ++ dependencies;
-          });
+          uvProject.script "nyl" { runtimeInputs = dependencies; };
+
+        packages.docs = let
+          docs = uv.loadProject {
+            workspaceRoot = ./docs;
+            python = pkgs.python312;
+          };
+          mkdocs = docs.command "mkdocs";
+        in pkgs.writeShellScriptBin "docs" ''
+          set -eu
+          cd docs
+          ${mkdocs} $@
+        '';
 
         # TODO: Use formatter.fmt, but it complains about missing type attribute
         # TODO: Have it also fmt the nix code
         formatter = pkgs.writeShellScriptBin "fmt" ''
           set -x
-          ${ruff} --config ${ruffConfig} format .
+          ${uvProject.devCommand "ruff"} --config ${ruffConfig} format .
           ${pkgs.nixfmt-classic}/bin/nixfmt .
         '';
+
+        # TODO: Do not require ruff/mypy to be dependencies of the project?
 
         packages.lint = pkgs.writeShellScriptBin "lint" ''
           set -x
           checkDir="''${1:-src}"
-          ${ruff} --config ${ruffConfig} check "${./.}/$checkDir"
-          ${ruff} --config ${ruffConfig} format --check "${./.}/$checkDir"
+          ${uvProject.devCommand "ruff"} --config ${ruffConfig} check "${
+            ./.
+          }/$checkDir"
+          ${
+            uvProject.devCommand "ruff"
+          } --config ${ruffConfig} format --check "${./.}/$checkDir"
           # TODO: If we don't copy the workdir to the nix store we get more Mypy errors :(
-          ${dmypy} run -- --config-file ${mypyConfig} "${./.}/$checkDir"
+          ${uvProject.devCommand "dmypy"} run -- --config-file ${mypyConfig} "${
+            ./.
+          }/$checkDir"
           ${pkgs.nixfmt-classic}/bin/nixfmt --check .
         '';
 
@@ -84,20 +92,22 @@
           dependenciesPath = nixpkgs.lib.concatStringsSep ":"
             (map (dep: "${dep}/bin") dependencies);
         in pkgs.writeShellScriptBin "test"
-        "PATH=\${PATH}:${dependenciesPath} ${pytest} ${./.}";
+        "PATH=\${PATH}:${dependenciesPath} ${uvProject.devCommand "pytest"} ${
+          ./.
+        }";
 
         checks.lint = pkgs.runCommand "lint" { } ''
           # TODO: Is it an issue that this runs the Mypy daemon?
-          ${self.packages.${system}.lint}/bin/lint
+          ${packages.lint}/bin/lint
           echo Done > $out
         '';
 
         checks.test = pkgs.runCommand "test" { } ''
-          ${self.packages.${system}.test}/bin/test
+          ${packages.test}/bin/test
           echo Done > $out
         '';
 
         devShells.default =
-          pkgs.mkShell { buildInputs = [ devEnv ] ++ dependencies; };
+          pkgs.mkShell { buildInputs = [ uvProject.venv.dev ]; };
       });
 }
